957dcc55f1cc3c6e4a142683c21f5efe87ad9177
uku <uku3lig@gmail.com>
2022-10-17 14:24:29 +0200

    initial commit

---
+++ src/main/java/net/uku3lig/mcibot/MciBotApplication.java
@@ -0,0 +1,13 @@
+package net.uku3lig.mcibot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MciBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MciBotApplication.class, args);
+    }
+
+}
