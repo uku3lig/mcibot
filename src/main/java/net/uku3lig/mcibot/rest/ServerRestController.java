a20a551f53ceaa6489c1d981565b15637f85095a
uku <uku3lig@gmail.com>
2022-10-19 23:52:06 +0200

    remove api route for server creation

--- src/main/java/net/uku3lig/mcibot/rest/ServerRestController.java
+++ src/main/java/net/uku3lig/mcibot/rest/ServerRestController.java
@@ -1,12 +1,13 @@
 package net.uku3lig.mcibot.rest;
 
-import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.uku3lig.mcibot.jpa.ServerRepository;
 import net.uku3lig.mcibot.model.Server;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
@@ -18,14 +19,5 @@ public class ServerRestController {
     @GetMapping("/server/{id}")
     public Server getServer(@PathVariable long id) {
         return repository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
-    }
-
-    @PostMapping("/server")
-    public Server createServer(@RequestBody @Valid Server server) {
-        if (repository.existsById(server.getDiscordId())) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "A server with this id already exists.");
-        }
-        repository.save(server);
-        return server;
     }
 }
