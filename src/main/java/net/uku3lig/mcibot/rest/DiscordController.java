2f7d36bd7c1fcb8bd64a5c0b0b30449f04a460a3
uku <uku3lig@gmail.com>
2022-10-19 23:51:36 +0200

    add very basic state integration

--- src/main/java/net/uku3lig/mcibot/rest/DiscordController.java
+++ src/main/java/net/uku3lig/mcibot/rest/DiscordController.java
@@ -2,6 +2,8 @@ package net.uku3lig.mcibot.rest;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.interactions.components.buttons.Button;
 import net.uku3lig.mcibot.MCIBot;
 import net.uku3lig.mcibot.config.Config;
 import net.uku3lig.mcibot.jpa.ServerRepository;
@@ -14,6 +16,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 @RestController
 @RequiredArgsConstructor
@@ -35,13 +40,21 @@ public class DiscordController {
     public ModelAndView discord(@RequestParam("guild_id") long guildId, @RequestParam String code, @RequestParam String state) {
         if (isDiscordBroken()) return Util.error("Client ID or secret not set");
 
+        String[] decodedState = new String(Base64.getDecoder().decode(state), StandardCharsets.UTF_8).split("\n");
+        long minecraftId = Long.parseLong(decodedState[0]);
+        String minecraftName = decodedState[1];
+
         discord.getAccessToken(code, "http://localhost:8080/discord")
                 .map(TokenResponse::getAccessToken)
                 .flatMap(discord::getUser)
                 .map(DiscordUser::getId)
-                .subscribe(log::info); // TODO dm the user and say hi :3
+                .subscribe(id -> MCIBot.getJda().retrieveUserById(id)
+                        .flatMap(User::openPrivateChannel)
+                        .flatMap(pc -> pc.sendMessageFormat("%s has requested to link the minecraft server to the discord server %s", minecraftName, guildId)
+                                .addActionRow(Button.secondary("lol", "haha cant click me").withDisabled(true)))
+                        .complete());
 
-        Server server = new Server(guildId, state == null ? 0 : Long.parseLong(state));
+        Server server = new Server(guildId, minecraftId);
         repository.save(server);
 
         return new ModelAndView("redirect:https://discord.com/oauth2/authorized");
