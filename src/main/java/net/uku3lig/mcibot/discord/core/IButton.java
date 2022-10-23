63a6c7d67e5f41df1f5d668bf52d8aa75ec2af92
uku <uku3lig@gmail.com>
2022-10-23 20:19:04 +0200

    make IButton an interface again

--- src/main/java/net/uku3lig/mcibot/discord/core/IButton.java
+++ src/main/java/net/uku3lig/mcibot/discord/core/IButton.java
@@ -2,29 +2,7 @@ package net.uku3lig.mcibot.discord.core;
 
 import discord4j.core.event.domain.interaction.ButtonInteractionEvent;
 import discord4j.core.object.component.Button;
-import reactor.core.publisher.Mono;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-
-public abstract class IButton implements InteractionHandler<ButtonInteractionEvent> {
-    private final List<String> args = new LinkedList<>();
-    
-    protected IButton(List<String> args) {
-        this.args.addAll(args);
-    }
-
-    public abstract Button getButton();
-
-    protected String getId(String id) {
-        return id + (args.isEmpty() ? "" : "," + String.join(",", args));
-    }
-
-    public abstract Mono<Void> onButtonClick(ButtonInteractionEvent event, List<String> args);
-
-    @Override
-    public final Mono<Void> onInteraction(ButtonInteractionEvent event) {
-        return onButtonClick(event, Arrays.stream(event.getCustomId().split(",")).skip(1).toList());
-    }
+public interface IButton extends InteractionHandler<ButtonInteractionEvent> {
+    Button getButton();
 }
