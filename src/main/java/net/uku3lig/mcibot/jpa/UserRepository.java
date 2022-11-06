d5305b649cb9a39e20c1dc0f59ecd533912b09ea
uku <uku3lig@gmail.com>
2022-11-06 02:56:07 +0100

    remove unused methods in UserRepository

--- src/main/java/net/uku3lig/mcibot/jpa/UserRepository.java
+++ src/main/java/net/uku3lig/mcibot/jpa/UserRepository.java
@@ -3,15 +3,15 @@ package net.uku3lig.mcibot.jpa;
 import net.uku3lig.mcibot.model.BlacklistedUser;
 import org.springframework.data.repository.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository extends Repository<BlacklistedUser, Long> {
     void save(BlacklistedUser user);
-    void delete(BlacklistedUser user);
 
     Optional<BlacklistedUser> findByDiscordAccountsContaining(long discordId);
     Optional<BlacklistedUser> findByMinecraftAccountsContaining(UUID minecraftId);
 
-    boolean existsByDiscordAccountsContaining(long discordId);
+    List<BlacklistedUser> findAll();
 }
