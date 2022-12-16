8615ede469f57e4721599a1e1b473161e87126d5
uku <uku3lig@gmail.com>
2022-12-16 12:46:49 +0100

    refactor main class slightly

--- src/main/java/net/uku3lig/mcibot/MCIBot.java
+++ src/main/java/net/uku3lig/mcibot/MCIBot.java
@@ -4,6 +4,7 @@ import discord4j.core.DiscordClientBuilder;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.rest.RestClient;
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import net.uku3lig.mcibot.config.Config;
 import net.uku3lig.mcibot.config.ConfigManager;
 import org.springframework.amqp.core.DirectExchange;
@@ -11,14 +12,18 @@ import org.springframework.amqp.core.Exchange;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
+import java.util.Arrays;
 
 @SpringBootApplication
+@Slf4j
 public class MCIBot {
     @Getter
     private static final ConfigManager<Config> manager = ConfigManager.create(Config.class, "mcibot_config");
@@ -29,6 +34,12 @@ public class MCIBot {
 
     public static void main(String[] args) {
         SpringApplication.run(MCIBot.class, args);
+        log.info("=== Finished loading! ===");
+    }
+
+    @Bean
+    public CommandLineRunner profileChecker(Environment env) {
+        return args -> log.info("Active profiles: {}", Arrays.toString(env.getActiveProfiles()));
     }
 
     @Bean
