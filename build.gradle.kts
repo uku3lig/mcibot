8d2d34a82b2153ca4dc493a099d162b7f48851f4
uku <uku3lig@gmail.com>
2023-05-05 12:00:40 +0200

    update d4j to 3.0.0-M1

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,7 +30,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-amqp")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
 
-    implementation("com.discord4j:discord4j-core:3.2.3")
+    implementation("com.discord4j:discord4j-core:3.3.0-M1")
     implementation("com.moandjiezana.toml:toml4j:0.7.2")
     implementation("org.reflections:reflections:0.10.2")
     runtimeOnly("org.mariadb.jdbc:mariadb-java-client:3.0.8")
